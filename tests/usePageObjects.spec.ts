270cefcedd05ce155e30106abac45459c31f95e2
Rocky Topna <rockytopna@macbookpro.mynetworksettings.com>
2025-10-01 14:04:14 -0400

    added faker lib for the test data

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { PageManager } from "../page-object/pageManager";
+import {faker} from "@faker-js/faker"
 
 
 test.beforeEach(async ({ page }) => {
@@ -19,9 +20,15 @@ await pm.navigateTo().tooltipPage()
 
 test('parameterized methods', async ({page})=> {
 const pm = new PageManager (page)
+//Using faker lib. to generate test data (name, pass, email etc)
+const randomFullName = faker.person.fullName()
+//added JS library to replace the space 
+const randomEmail = `${randomFullName.replace(' ','')}${faker.number.int(1000)}@test.com`
+
 await pm.navigateTo().formLayoutPage();
 await pm.onFormLayoutPage().submitUsingTheGridFormwithCredentialAndSelectOption('test@test.com','Welcome', 'Option 2')
-await pm.onFormLayoutPage().sumbitInlineFormWithNameEmailAndCheckbox('John Smith', 'Johnsmith@test.com', true)
+//Here we added the faker random fullname, email 
+await pm.onFormLayoutPage().sumbitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, true)
 await pm.navigateTo().datePickerPage()
 await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(10);
 await pm.onDatePickerPage().selectDatepickerWithRangeFromToday(5,15)
